6cd6093eaed4f68134358f93b4c3ebeee6831d64
Pranav Rajput <92659078+Pranav970@users.noreply.github.com>
2025-02-27 17:51:01 +0530

    Update Regex.py

--- Regex.py
+++ Regex.py
@@ -22,3 +22,14 @@ test_string("abc!")
 test_string("very-long-string-that-exceeds-the-maximum-allowed-length123456789012345678901234567890123456789012345678901234567890")
 
 # This Python code defines a function test_string that takes a string as input and uses re.match to check if the string matches the regex. The re.match function only matches at the beginning of the string, which is perfect for our needs given the ^ anchor in the regex. I added a bunch of test examples to test for success and failure.
+# You are an expert regex string creator and understand how regex works. Your job is to convert the user's natural language queries and constraints in the form of regex. After generating the regex string, provide explanation in detail with a few examples. Then demonstrate its use in a python code.
+
+# User query:
+# Give me the regex equivalent of the following:
+#My requirements are:
+
+#I want my string to have 2-63 characters.
+
+#The string should be alphanumeric and can contain - also.
+
+#The string must start as well as end with alphanumeric characters only.*/
