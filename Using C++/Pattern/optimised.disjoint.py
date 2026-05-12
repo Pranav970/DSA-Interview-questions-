62d4debc284b0dcbafd18daf6d991b42606a8c3b
Pranav Rajput <92659078+Pranav970@users.noreply.github.com>
2026-05-12 23:37:06 +0530

    Create optimised.disjoint.py

---
+++ Using C++/Pattern/optimised.disjoint.py
@@ -0,0 +1,15 @@
+class DSU:
+    def __init__(self, n):
+        self.p = list(range(n))
+
+    def find(self, x):
+        if self.p[x] != x:
+            self.p[x] = self.find(self.p[x])
+        return self.p[x]
+
+    def union(self, a, b):
+        pa, pb = self.find(a), self.find(b)
+        if pa == pb:
+            return False
+        self.p[pb] = pa
+        return True
