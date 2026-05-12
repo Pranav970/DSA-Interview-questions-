fe8591c5364247f63d21edb0303408cb9aa3e7ba
Pranav Rajput <92659078+Pranav970@users.noreply.github.com>
2026-05-12 23:36:01 +0530

    Update disjoint-set.py

--- Using C++/Pattern/disjoint-set.cpp
+++ Using C++/Pattern/disjoint-set.cpp
@@ -1,1 +1,35 @@
+class DisjointSet:
+    def __init__(self, n):
+        self.parent = [i for i in range(n)]
+        self.rank = [0] * n
+        # Optional: size array
+        self.size = [1] * n
 
+    # Path Compression
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    # Union by Rank
+    def union(self, x, y):
+        px = self.find(x)
+        py = self.find(y)
+
+        if px == py:
+            return False
+
+        if self.rank[px] < self.rank[py]:
+            self.parent[px] = py
+            self.size[py] += self.size[px]
+
+        elif self.rank[px] > self.rank[py]:
+            self.parent[py] = px
+            self.size[px] += self.size[py]
+
+        else:
+            self.parent[py] = px
+            self.rank[px] += 1
+            self.size[px] += self.size[py]
+
+        return True
